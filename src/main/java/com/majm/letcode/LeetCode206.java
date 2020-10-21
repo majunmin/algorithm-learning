a1878069ecb4caf1181f39e865d2881ab6e5779c
majm <majm@ushareit.com>
2020-10-22 00:13:33 +0800

    [algorithm][add][leetCode206 reverseList]

--- src/main/java/com/majm/letcode/LeetCode206.java
+++ src/main/java/com/majm/letcode/LeetCode206.java
@@ -21,6 +21,38 @@ public class LeetCode206 implements Solution {
      */
     @Override
     public ListNode reverseList(ListNode head) {
-        return null;
+        // 1. 遍历链表
+
+        // 1. 构建一个新链表(往链表的头部插值很容易)
+        ListNode newNode = null;
+        //
+        ListNode oldNode;
+        while(head.next != null){
+            // 保存head指针
+            oldNode = head;
+            // 记录head指针
+            head = head.next;
+            // 往新链表头部插值
+            oldNode.next = newNode;
+            // 更新新链表头
+            newNode = oldNode;
+        }
+        return newNode;
+
+
+
+        // 2. 递归
+
+        // 递归终点
+//        if (head == null || head.next == null){
+//            return head;
+//        }
+//        //
+//        ListNode newHead = reverseList(head.next);
+//        //
+//        head.next.next = head;
+//        head.next = null;
+//        return  newHead;
+
     }
 }
